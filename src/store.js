8efec140ecb44d0d8e6352cdd5cf821914d4132d
maneptha <maneptha@gmail.com>
2017-07-31 10:28:47 +0200

    build update

--- src/store.js
+++ src/store.js
@@ -20,12 +20,9 @@ const middlewares = [
   epicMiddleware,
 ];
 
-// if (process.env.NODE_ENV !== 'production') {
-middlewares.push(logger);
-// }
-console.log('\n\nlogging starts\n\n',
-middlewares
-, '\n\nlogging ends\n\n');
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(logger);
+}
 
 const store = createStore(reducers, applyMiddleware(...middlewares));
 
